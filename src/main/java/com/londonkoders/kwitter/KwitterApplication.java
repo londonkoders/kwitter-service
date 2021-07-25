324ef842f4a18c74e68e086c62565077bd51dd68
jihyun-um <ashleyum20@gmail.com>
2021-07-25 17:47:16 +0100

    Set up project with Java 11 and SpringBoot

---
+++ src/main/java/com/londonkoders/kwitter/KwitterApplication.java
@@ -0,0 +1,13 @@
+package com.londonkoders.kwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/londonkoders/kwitter/KwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.londonkoders.kwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
